6b931f62592b3b59a86cbed0f731b1f4beb71d6c
EAntoshchuk <114941039+EAntoshchuk@users.noreply.github.com>
2023-07-03 22:35:51 +0000

    Deploying to gh-pages from @ EAntoshchuk/goit-react-hw-08-phonebook@0c9f6c32c8e207f7033f89c83a8f54772c831b4b 🚀

---
+++ static/js/795.96ca60f6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[795],{795:function(e,n,o){o.r(n),o.d(n,{default:function(){return h}});var t=o(184);function h(){return(0,t.jsx)("div",{children:(0,t.jsx)("h2",{children:"PhoneBook"})})}}}]);
+//# sourceMappingURL=795.96ca60f6.chunk.js.map
